4c1a0fdf7c8bbdbc560ceff5fe0dd3f5ba7b4f3c
YagubHajizada <yagubhajizada9@gmail.com>
2021-02-27 10:23:55 +0200

    .

--- src/com/company/Homework2.java
+++ src/com/company/Homework2.java
@@ -11,11 +11,11 @@ public class Homework2 {
         int y = scanner.nextInt();
         scanner.close();
         if (x > -2 && x < 3 && y > -4 && y < 1) {
-            System.out.println("Koordinātas" + "(" + x + ";" + y + ")" + "atrodas uz figūras iekšienē!");
+            System.out.println("Koordinātas (" + x + "; " + y + ") atrodas uz figūras iekšienē!");
         } else if (x >= -2 && y >= -4 && x <= 3 && y <= 1) {
-            System.out.println("Koordinātas" + "(" + x + ";" + y + ")" + "atrodas uz figūras līnijas!");
+            System.out.println("Koordinātas (" + x + "; " + y + ") atrodas uz figūras līnijas!");
         } else {
-            System.out.println("Koordinātas" + "(" + x + ";" + y + ")" + "atrodas figūras ārpusē!");
+            System.out.println("Koordinātas (" + x + "; " + y + ") atrodas figūras ārpusē!");
         }
     }
 }
